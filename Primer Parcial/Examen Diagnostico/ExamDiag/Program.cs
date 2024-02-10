b1adea9933c1b99e1fc7be01d522aede4134da23
Aridr1z <chonpi99@gmail.com>
2024-02-09 21:26:28 -0600

    Examen Diag
    Mi primer summit de mi examen diag xd

---
+++ Primer Parcial/Examen Diagnostico/ExamDiag/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Timers;
+
+//Creo la clase animal
+class Animal 
+{
+
+    public virtual void Sonido()
+    
+    {
+        Console.WriteLine("El animal hace un sonido generico");
+
+    }
+
+}
+
+
+//Clase que va heredar el metodo de Animal que es hacer sonido
+
+class Perro : Animal
+{
+
+    public override void Sonido()
+
+    {
+        Console.WriteLine("El perro dice guau");
+            
+    }
+
+
+}
+
+//Clase que va heredar el metodo de Animal que es hacer sonido
+
+class Gato : Animal 
+
+{
+
+    public override void Sonido()
+
+    {
+        Console.WriteLine("El gato dice miau");
+
+    }
+
+}
+
+
+
+
+class Codigo
+
+{
+
+    static void Main(string[] args)
+    {
+
+        Animal animal = new Animal();
+
+        Perro perro = new Perro();
+
+        Gato gato = new Gato();
+
+        Console.WriteLine("Sonido de animal genérico:");
+        animal.Sonido();
+
+        Console.WriteLine("\nSonido de un perro:");
+        perro.Sonido();
+
+        Console.WriteLine("\nSonido de un gato:");
+        gato.Sonido();
+
+    }
+
+
+}
